178b96ce723fc7694e6e8caa765a1ba0acc254d9
Grant Kirill <webdotg.inbox@gmail.com>
2024-03-24 00:01:43 +0300

    add base drag card animate

--- src/components/main/cost/Details.tsx
+++ src/components/main/cost/Details.tsx
@@ -1,9 +1,27 @@
 import style from './details.module.scss';
+import Img from "../../../PNG/Symbol.png";
+import Marginer  from "./marginer/marginer";
 
 export default function Details() {
   return (
     <section className={style['details']}>
-      Deatails
+
+<div className={style.detailsContainer}>
+      <span className={style.smallText}>LOGO</span>
+      <div className={style.spacedHorizontalContainer}>
+        <span className={style.mediumText}>пакет такой то охуенный</span>
+        <span className={style.mediumText}>$1</span>
+      </div>
+      <Marginer direction="vertical" margin="1.2em" />
+      <div className={style.spacedHorizontalContainer}>
+        <span className={style.mediumText}>смотри</span>
+        <button className={style.buyButton}>покупай</button>
+      </div>
+      <div className={style.Logo}>
+        <img src={Img} alt="img" />
+      </div>
+    </div>
+
 
     </section>
   )
